22357f07d1ca9ecea04fc801911646f5f8d46e37
Bartho Bernsmann <Bartho@outlook.com>
2018-02-19 18:34:47 -0300

    Added the shopping cart class

---
+++ PhotosOfUs.Model/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotosOfUs.Model.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string CartCode { get; set; }
+        public int PhotoId { get; set; }
+        public int Count { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual Photo Photo { get; set; }
+    }
+}
