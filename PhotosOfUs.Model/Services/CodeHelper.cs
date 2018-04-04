5297323888d54cf45d3b2dc7262e7ff8990e11e3
Fernando Rodrigues <ferd-rodrigues@hotmail.com>
2018-04-04 19:00:46 -0300

    Bad word filter added to code generation

--- PhotosOfUs.Model/Services/CodeHelper.cs
+++ PhotosOfUs.Model/Services/CodeHelper.cs
@@ -11,7 +11,7 @@ namespace PhotosOfUs.Model.Services
 
         public static string NewCode(int length)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const string chars = "BCDFGHJKLMNPQRSTVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
