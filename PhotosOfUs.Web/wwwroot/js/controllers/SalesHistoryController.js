475172de97ae9148b888303e5350651f45a0194a
Lars Krakhecke <lars.krakhecke@googlemail.com>
2018-04-05 18:04:57 +0200

    Added an angular controller for the sales history page and hooked up the search sales history back end

---
+++ PhotosOfUs.Web/wwwroot/js/controllers/SalesHistoryController.js
@@ -0,0 +1,14 @@
+﻿app.controller('SalesHistoryCtrl', ['$scope', '$window', '$location', '$http', ($scope, $window, $location, $http) => {
+
+    $scope.query = "";
+
+    $scope.querySalesHistory = (query) => {
+        $('.sales-container .overlay').addClass('loading');
+        $http.get('/api/PhotographerApi/SalesHistory?query=' + query).then(x => {
+            if (x.status === 200) {
+                $('.sales-content').html(x.data);
+                $('.sales-container .overlay').removeClass('loading');
+            }
+        });
+    }
+}])
