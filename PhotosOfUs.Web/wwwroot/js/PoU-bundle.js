fced01cbe45984dd7e91f99eb78437f94ef994d3
Joseph Young <joseph@josephayoung.com>
2018-04-15 09:08:24 -0500

    Small bug fixes

--- PhotosOfUs.Web/wwwroot/js/PoU-bundle.js
+++ PhotosOfUs.Web/wwwroot/js/PoU-bundle.js
@@ -1,14 +1,10 @@
 'use strict';
 
-var app = angular.module('app', ['ngMaterial', 'angularFileUpload', 'monospaced.elastic', 'ui.bootstrap']);
+var app = angular.module('app', ['ngMaterial', 'angularFileUpload', 'monospaced.elastic']);
 
 
 
-app.filter('startFrom', function () {
-    return function (data, start) {
-        return data.slice(start)
-    }
-})
+
 
 app.factory('photoApi', [
     '$http', '$rootScope', function ($http, $rootScope) {
@@ -471,8 +467,7 @@ angular.module('app').controller('UploadController', function ($scope, $http, Fi
 app.controller('CardCtrl', ['$scope', '$rootScope', '$window', '$mdDialog', 'photoApi', 'cardApi', ($scope, $rootScope, $window, $mdDialog, photoApi, cardApi) => {
     $scope.close = () => $mdDialog.hide();
     $scope.cards = [];
-    $scope.pageSize = 5;
-    $scope.currentPage = 1;
+
     $scope.initCardCtrl = function () {
         
         cardApi.getAll()
@@ -668,4 +663,60 @@ app.controller('PhotographerAccountCtrl', ['$scope', '$window', '$location', '$h
 
     }
 
+}])
+app.controller('CardCtrl', ['$scope', '$rootScope', '$window', '$mdDialog', 'photoApi', 'cardApi', ($scope, $rootScope, $window, $mdDialog, photoApi, cardApi) => {
+    $scope.close = () => $mdDialog.hide();
+    $scope.cards = [];
+
+    $scope.initCardCtrl = function () {
+        
+        cardApi.getAll()
+            .then(function (x) {
+                angular.forEach(x.data, function (c) { $scope.cards.push(c); });
+                console.log(JSON.stringify(x.data));
+            })
+    }
+
+    $scope.exportMultipleCardsModal = function() {
+        $mdDialog.show({
+            templateUrl: '/Photographer/MultipleCardsModal',
+            controller: 'CardCtrl',
+            clickOutsideToClose: true,
+        })
+    }
+
+    $scope.exportMultipleCards = function (quantity) {
+        location.href = "/Photographer/ExportMultipleCards/?quantity=" + quantity;
+    }
+
+    $scope.addFolder = function (folderName) {
+        folderApi.add(folderName)
+            .then(function (x) {
+                //adds to list view
+                $scope.close();
+                $rootScope.$broadcast('FolderAdded', x.data);
+            })
+    }
+
+    $scope.$on('FolderAdded', function (e, folder) {
+
+        console.log('added folder - ' + JSON.stringify(folder));
+
+        $scope.folders.push(folder);
+
+    });
+}])
+app.controller('SalesHistoryCtrl', ['$scope', '$window', '$location', '$http', ($scope, $window, $location, $http) => {
+
+    $scope.query = "";
+
+    $scope.querySalesHistory = (query) => {
+        $('.sales-container .overlay').addClass('loading');
+        $http.get('/api/PhotographerApi/SalesHistory?query=' + query).then(x => {
+            if (x.status === 200) {
+                $('.sales-content').html(x.data);
+                $('.sales-container .overlay').removeClass('loading');
+            }
+        });
+    }
 }])
