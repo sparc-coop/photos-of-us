ac6afeb1afa25cb1614fd6f4ec508125f00ef4d1
Layla Hedges <layla.hedges@gmail.com>
2018-08-15 14:40:58 -0400

    Email signup set up with mailchimp

--- PhotosOfUs.Web/Controllers/ContactController.cs
+++ PhotosOfUs.Web/Controllers/ContactController.cs
@@ -3,25 +3,39 @@ using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
+using MailChimp.Net;
+using MailChimp.Net.Models;
 
 namespace PhotosOfUs.Web.Controllers
 {
     public class ContactController : Controller
     {
         public IConfiguration Configuration { get; }
+    
 
-        [HttpPost]
+    [HttpPost]
         public async Task<IActionResult> SendLaunchEmail(ContactViewModel model)
         {
+            //var apiKey = "";
+            //var client = new SendGridClient(apiKey);
+            //var from = new EmailAddress(model.FromEmail);
+            //var subject = $"Photos Of Us Launch Contact Email";
+            //var to = new EmailAddress("marketing@kuviocreative.com");
+            //var plainTextContent = model.FromEmail;
+            //var htmlContent = $"Add to Contact List: {model.FromEmail}";
+            //var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            //await client.SendEmailAsync(msg);
+
+            //return Redirect("/");
+
             var apiKey = "";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress(model.FromEmail);
-            var subject = $"Photos Of Us Launch Contact Email";
-            var to = new EmailAddress("marketing@kuviocreative.com");
-            var plainTextContent = model.FromEmail;
-            var htmlContent = $"Add to Contact List: {model.FromEmail}";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            await client.SendEmailAsync(msg);
+            var mailChimpManager = new MailChimpManager(apiKey);
+            var listId = "";
+            var email = model.FromEmail;
+            var member = new Member { EmailAddress = email, StatusIfNew = Status.Subscribed };
+            //member.MergeFields.Add("FNAME", "FirstName");
+            //member.MergeFields.Add("LNAME", "LastName");
+            await mailChimpManager.Members.AddOrUpdateAsync(listId, member);
 
             return Redirect("/");
         }
