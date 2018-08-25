3995cb4d8cbb786965dedfc58e0a4be4bebf1fb0
Fernando Rodrigues <ferd-rodrigues@hotmail.com>
2018-08-25 20:31:56 -0300

    Fix the autocomplete on Search page to reflect on all tags

--- PhotosOfUs.Web/wwwroot/js/PoU-bundle.js
+++ PhotosOfUs.Web/wwwroot/js/PoU-bundle.js
@@ -1111,10 +1111,10 @@ app.controller('CardCtrl', ['$scope', '$rootScope', '$window', '$mdDialog', 'pho
 
     });
 }])
-app.controller('PhotographerCtrl', ['$scope', '$window', '$location', '$http', '$mdDialog', 'photographerApi', 'userApi', ($scope, $window, $location, $http, $mdDialog, photographerApi, userApi) => {
+app.controller('PhotographerCtrl', ['$scope', '$window', '$location', '$http', '$mdDialog', '$filter', '$timeout', 'photographerApi', 'userApi', ($scope, $window, $location, $http, $mdDialog, $filter, $timeout, photographerApi, userApi) => {
 
     $scope.tags = [];
-    $scope.loadedtags = [];
+    $scope.allTags = [];
     $scope.isBulkEditEnabled = false;
     $scope.selectedPhotos = [];
     $scope.profilePhotos = [];
@@ -1187,12 +1187,18 @@ app.controller('PhotographerCtrl', ['$scope', '$window', '$location', '$http', '
     //    });
     //};
 
-    $scope.loadTags = function () {
+    $scope.getAllTags = function () {
         $http.get('/api/Photo/GetAllTags/')
             .then(function (x) {
-                angular.forEach(x.data, function (f) { $scope.loadedtags.push(f); });
+                angular.forEach(x.data, function (f) { $scope.allTags.push(f); });
                 console.log(JSON.stringify(x.data));
             });
+    };
+
+    $scope.loadTags = function (query) {
+        return $timeout(function () {
+            return $filter('filter')($scope.allTags, query);
+        });
     };
 
     $scope.getSearchString = function (searchterms) {
