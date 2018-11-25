e5fd272f611d4feb9541629b39d4575c2a60bb30
Joseph Young <joseph@josephayoung.com>
2018-11-25 14:14:59 -0600

    Replaced BrandAccount with Event

--- PhotosOfUs.Model/Events/Event.cs
+++ PhotosOfUs.Model/Events/Event.cs
@@ -5,30 +5,28 @@ using System.Security.Claims;
 
 namespace PhotosOfUs.Model.Models
 {
-    public partial class BrandAccount
+    public partial class Event
     {
-        public int BrandAccountId { get; set; }
+        public int EventId { get; set; }
         public int UserId { get; set; }
-
-        public int SeparatorThickness { get; set; }
-        public int SeparatorWidth { get; set; }
-        public int CodeInputStyle { get; set; }
-        public int BrandingStyle { get; set; }
-
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string PageTitle { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
         public string HomepageTemplate { get; set; }
         public string PersonalLogoUrl { get; set; }
         public string FeaturedImageUrl { get; set; }
         public string OverlayColorCode { get; set; }
-        public string OverlayOpacity { get; set; }
+        public decimal OverlayOpacity { get; set; }
         public string AccentColorCode { get; set; }
         public string BackgroundColorCode { get; set; }
         public string HeaderColorCode { get; set; }
         public string BodyColorCode { get; set; }
-        public string PageTitle { get; set; }
-        public string Description { get; set; }
         public string SeparatorStyle { get; set; }
-
-        public User User { get; set; }
+        public int SeparatorThickness { get; set; }
+        public int SeparatorWidth { get; set; }
+        public int BrandingStyle { get; set; }
 
         public enum HomepageTemplates
         {
@@ -50,11 +48,11 @@ namespace PhotosOfUs.Model.Models
             None
         }
 
-        public BrandAccount CreateDefaultBrandAccount(int id)
+        public Event CreateDefaultBrandAccount(int id)
         {
             UserId = id;
-            BrandAccountId = id;
-            HomepageTemplate = BrandAccount.HomepageTemplates.TwoOneSplit.ToString();
+            EventId = id;
+            HomepageTemplate = Event.HomepageTemplates.TwoOneSplit.ToString();
             PersonalLogoUrl = "";
             BackgroundColorCode = "F6FFFF";
             AccentColorCode = "FF6060";
@@ -62,13 +60,13 @@ namespace PhotosOfUs.Model.Models
             BodyColorCode = "000000";
             FeaturedImageUrl = "";
             OverlayColorCode = "194952";
-            OverlayOpacity = "50";
+            OverlayOpacity = 0.5M;
             PageTitle = "My Page Title";
             Description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.";
-            SeparatorStyle = BrandAccount.SeparatorStyles.StraightLine.ToString();
+            SeparatorStyle = Event.SeparatorStyles.StraightLine.ToString();
             SeparatorThickness = 1;
             SeparatorWidth = 40;
-            BrandingStyle = (int)BrandAccount.FeatureTypes.Dark;
+            BrandingStyle = (int)Event.FeatureTypes.Dark;
 
             return this;
         }
