ca8b7266ce0d7cf840361688506f32d1858990e5
Joseph Young <joseph@josephayoung.com>
2018-12-31 13:43:57 -0600

    Added TSLint and TS-ified Startup

--- PhotosOfUs.Web/wwwroot/js/PoU.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-var app = angular.module('app', ['ngMaterial', 'angularFileUpload', 'monospaced.elastic', '720kb.socialshare', 'ui.bootstrap', 'ngTagsInput', 'angular.filter']);
-
-app.filter('startFrom', function () {
-    return function (data, start) {
-        return data.slice(start)
-    }
-})
---
+++ PhotosOfUs.Web/wwwroot/js/Startup.ts
@@ -0,0 +1,15 @@
+﻿import angular from 'angular';
+
+angular.module('app', [
+    'ngMaterial',
+    'angularFileUpload',
+    'monospaced.elastic',
+    '720kb.socialshare',
+    'ui.bootstrap',
+    'ngTagsInput',
+    'angular.filter'
+]);
+
+angular.module('app').filter('startFrom', () => {
+    return (data, start) => data.slice(start);
+});
