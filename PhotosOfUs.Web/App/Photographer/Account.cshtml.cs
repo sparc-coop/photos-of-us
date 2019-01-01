6bfcaa1b29e5c0e6934a1180fa5cdca1479787a5
Joseph Young <joseph@josephayoung.com>
2019-01-01 14:31:34 -0600

    TSify AccountController

---
+++ PhotosOfUs.Web/App/Photographer/Account.cshtml.cs
@@ -0,0 +1,41 @@
+using Kuvio.Kernel.Architecture;
+using Kuvio.Kernel.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PhotosOfUs.Model.Models;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PhotosOfUs.Pages.Photographer
+{
+    [Authorize]
+    public class AccountModel : PageModel
+    {
+        private readonly IRepository<User> _users;
+
+        public AccountModel(IRepository<User> users)
+        {
+            _users = users;
+        }
+
+        public User Photographer { get; private set; }
+
+        public IActionResult OnGet()
+        {
+           Photographer = _users.Find(x => x.Id == User.ID());
+           if (Photographer == null) return NotFound();
+           
+           return Page();
+        }
+
+        public IActionResult OnPost(User user)
+        {
+            var photographer = _users.Find(x => x.Id == User.ID());
+            photographer.UpdateProfile(user.Email, user.FirstName, user.LastName, user.DisplayName, user.JobPosition, user.ProfilePhotoUrl, user.Bio);
+            _users.Commit();
+            
+            return Page();
+        }
+    }
+}
---
+++ PhotosOfUs.Web/App/Photographer/AccountController.ts
@@ -0,0 +1,67 @@
+﻿namespace PhotosOfUs {
+    angular.module('app').controller('AccountCtrl',
+        ['$scope', '$window', '$mdDialog', 'UserApiClient', 'PhotographerApiClient',
+        ($scope, $window, $mdDialog, UserApiClient: UserApiClient, PhotographerClient: PhotographerClient) => {
+            $scope.originalSettings = {};
+
+            $scope.initBrandSettings = function () {
+                photographerApi.getBrandSettings().then(function (x) {
+                    $scope.brandSettings = x.data;
+                });
+            };
+
+            $scope.saveBrandSettings = function () {
+                photographerApi.saveBrandSettings($scope.brandSettings).then(function (x) {
+                    $scope.brandSettings = x.data;
+                });
+            };
+
+            $scope.discardChanges = () => window.location.reload();
+
+            $scope.saveAccountSettings = (ev) => ev.closest('form').submit();
+
+            $scope.uploadProfileImage = () => {
+                $mdDialog.show({
+                    templateUrl: '/Photographer/UploadProfileImage',
+                    controller: 'UploadProfileImageCtrl',
+                    clickOutsideToClose: true,
+                });
+            };
+
+            $scope.close = () => $mdDialog.hide();
+
+            $scope.deactivateModal = (option: string) => {
+                if (option === 'true') {
+                    $mdDialog.show({
+                        templateUrl: '/Photographer/Modals/DeactivateModal',
+                        scope: $scope,
+                        controller: 'ModalCtrl',
+                        clickOutsideToClose: true
+                    });
+                } else if (option === 'false') {
+                    $scope.reactivateAccount();
+                }
+            };
+
+            $scope.reactivateAccount = () => {
+                UserApiClient.reactivateAccount().then(function () { $window.location.reload(); });
+            };
+
+            $scope.deactivateAccount = () => {
+                UserApiClient.deactivateAccount().then(function () { $window.location.reload(); });
+            };
+
+            $scope.selected = 'details';
+
+            $scope.setSelected = (selected) => {
+                $scope.selected = selected;
+            };
+
+            $scope.templateSelected = '';
+
+            $scope.selectTemplate = (id) => {
+                $scope.templateSelected = id;
+            };
+
+        }]);
+    }
