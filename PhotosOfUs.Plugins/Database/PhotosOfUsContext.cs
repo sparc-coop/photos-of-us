b9aaeed20ce7f364d06651b118631e9e84af9e00
Joseph Young <joseph@josephayoung.com>
2019-02-10 14:06:55 -0600

    Switching to owned entities for DDD

--- PhotosOfUs.Plugins/Database/PhotosOfUsContext.cs
+++ PhotosOfUs.Plugins/Database/PhotosOfUsContext.cs
@@ -13,7 +13,7 @@ namespace PhotosOfUs.Model.Models
         public virtual DbSet<Event> Events { get; set; }
 
         public PhotosOfUsContext(DbContextOptions<PhotosOfUsContext> options) : base(options)
-        { 
+        {
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
@@ -23,35 +23,16 @@ namespace PhotosOfUs.Model.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<SocialMedia>(entity =>
-            {
-                entity.Property(e => e.AzureId).HasColumnName("AzureID");
-                entity.Property(e => e.Type).HasColumnName("Type");
-            });
-
-            modelBuilder.Entity<Card>(entity =>
-            {
-                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
-            });
-
-            modelBuilder.Entity<Folder>(entity =>
-            {
-                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
-            });
-
             modelBuilder.Entity<Order>(entity =>
             {
                 entity.ToTable("Order");
                 entity.Property(e => e.OrderDate).HasColumnType("datetime");
                 entity.Property(e => e.Total).HasColumnType("decimal(19, 4)");
-                //entity.Ignore(x => x.Amount);
-                //entity.Ignore(x => x.TotalPaid);
-                //entity.Ignore(x => x.Earning);
-            });
 
-            modelBuilder.Entity<OrderDetail>(entity =>
-            {
-                entity.Property(e => e.UnitPrice).HasColumnType("decimal(19, 4)");
+                entity.OwnsMany(e => e.OrderDetail, x =>
+                {
+                    x.Property(y => y.UnitPrice).HasColumnType("decimal(19, 4)");
+                });
             });
 
             modelBuilder.Entity<Photo>(entity =>
@@ -65,6 +46,12 @@ namespace PhotosOfUs.Model.Models
                 entity.Ignore(x => x.Resolution);
                 entity.Ignore(x => x.ThumbnailUrl);
                 entity.Ignore(x => x.WaterMarkUrl);
+
+                entity.OwnsMany(e => e.PhotoTag, x =>
+                {
+                    x.HasKey(y => new { y.PhotoId, y.TagId });
+                    x.Property(y => y.RegisterDate).HasColumnType("datetime");
+                });
             });
 
             modelBuilder.Entity<User>(entity =>
@@ -73,6 +60,27 @@ namespace PhotosOfUs.Model.Models
                 entity.Property(e => e.Id).HasColumnName("ID");
                 entity.Property(e => e.AzureId).HasColumnName("AzureID");
                 entity.Property(e => e.CreateDate).HasColumnType("datetime");
+
+                entity.OwnsMany(e => e.UserIdentities, x =>
+                {
+                    x.HasKey(y => y.AzureID);
+                    x.Property(y => y.AzureID)
+                        .ValueGeneratedNever();
+
+                    x.Property(y => y.CreateDate).HasColumnType("datetime");
+                    x.Property(y => y.LastLoginDate).HasColumnType("datetime");
+                });
+
+                entity.OwnsMany(e => e.SocialMedia, x =>
+                {
+                    x.Property(y => y.AzureId).HasColumnName("AzureID");
+                    x.Property(y => y.Type).HasColumnName("Type");
+                });
+
+                entity.OwnsMany(e => e.Folders, x =>
+                {
+                    x.Property(y => y.CreatedDate).HasColumnType("datetime");
+                });
             });
 
             modelBuilder.Entity<Event>(entity =>
@@ -80,16 +88,10 @@ namespace PhotosOfUs.Model.Models
                 entity.ToTable("Event");
                 entity.Property(e => e.EventId).HasColumnName("EventID");
                 entity.Property(e => e.UserId).HasColumnName("UserID");
-            });
-
-            modelBuilder.Entity<UserIdentity>(entity =>
-            {
-                entity.HasKey(e => e.AzureID);
-                entity.Property(e => e.AzureID)
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.CreateDate).HasColumnType("datetime");
-                entity.Property(e => e.LastLoginDate).HasColumnType("datetime");
+                entity.OwnsMany(e => e.Cards, card =>
+                {
+                    card.Property(x => x.CreatedDate).HasColumnType("datetime");
+                });
             });
 
             modelBuilder.Entity<PrintType>();
@@ -97,12 +99,6 @@ namespace PhotosOfUs.Model.Models
             modelBuilder.Entity<PrintPrice>();
 
             modelBuilder.Entity<Tag>();
-
-            modelBuilder.Entity<PhotoTag>(entity =>
-            {
-                entity.HasKey(x => new { x.PhotoId, x.TagId });
-                entity.Property(e => e.RegisterDate).HasColumnType("datetime");
-            });
         }
     }
 }
