1c27f4fedf317ca589923853edccdf2e85bcb9d5
Joseph Young <joseph@josephayoung.com>
2019-06-22 13:05:55 -0500

    Color Picker component

---
+++ PhotosOfUs.App/Shared/Components/ColorPicker.js
@@ -0,0 +1,10 @@
+﻿window.initColorPicker = function (el, scope) {
+    const pickr = Pickr.create({
+        el: el,
+        theme: 'nano'
+    });
+    pickr.on('save', function (color) {
+        scope.invokeMethodAsync('SetColor', color.toHEXA().toString());
+    });
+    return pickr;
+};
